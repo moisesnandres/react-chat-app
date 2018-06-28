05b60ada0b298bc882cc64239fc662362b51485a
Moisés Ñañez <moisesnandres@gmail.com>
2018-06-28 16:01:20 -0500

    Add autoscroll when send a message

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Message from './Message';
 
-const MessageList = ({ messages }) => (
-  <div className="message__list">
-    {messages.map(message => (
-      <Message
-        key={message.id}
-        username={message.senderId}
-        text={message.text}
-      />
-    ))}
-  </div>
-);
+class MessageList extends Component {
+  scrollDiv = React.createRef();
+
+  componentDidUpdate() {
+    this.scrollDiv.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  render() {
+    return (
+      <div className="message__list">
+        <div>
+          {this.props.messages.map(message => (
+            <Message
+              key={message.id}
+              username={message.senderId}
+              text={message.text}
+            />
+          ))}
+        </div>
+        <div className="message__list__empty" ref={this.scrollDiv} />
+      </div>
+    );
+  }
+}
 
 export default MessageList;
