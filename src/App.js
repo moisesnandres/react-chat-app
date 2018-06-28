e66466e343fc333ffbd995cc8ce5b9ab62123b60
Moisés Ñañez <moisesnandres@gmail.com>
2018-06-28 01:09:35 -0500

    Connect to Chatkit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import Chatkit from '@pusher/chatkit';
 import RoomList from './components/RoomList';
 import MessageList from './components/MessageList';
 import NewRoomForm from './components/NewRoomForm';
 import SendMessageForm from './components/SendMessageForm';
+import { tokenUrl, instanceLocator } from './config';
 import './styles.css';
 
 class App extends Component {
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator,
+      userId: 'icaJS',
+      tokenProvider: new Chatkit.TokenProvider({ url: tokenUrl })
+    });
+
+    chatManager.connect().then(currentUser => {
+      currentUser.subscribeToRoom({
+        roomId: 10447031,
+        hooks: {
+          onNewMessage: message => {
+            console.log(message.text);
+          }
+        }
+      });
+    });
+  }
+
   render() {
     return (
       <div className="app">
---
+++ src/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = process.env.REACT_APP_TOKEN_URL;
+const instanceLocator = process.env.REACT_APP_INSTANCE_LOCATOR;
+
+export { tokenUrl, instanceLocator };
